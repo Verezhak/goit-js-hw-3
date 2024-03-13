d6ff3fe41602377559046a7b048f04d434fdd54a
Verezhak <153289346+Verezhak@users.noreply.github.com>
2024-03-13 05:19:40 +0100

    updated files

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,18 @@
-function getElementWidth(content, padding, border) {
-     const contentWidth = Number.parseFloat(content);
-    const paddingWidth = Number.parseFloat(padding);
-    const borderWidth = Number.parseFloat(border);
-    const totalWidth = contentWidth + paddingWidth * 2 + borderWidth * 2;
-       return totalWidth;
+function filterArray(numbers, value) {
+   const newArray = [];
+   for (number of numbers) {
+      if (number > value) {
+         newArray.push(number);
+      }
+   }
+   return newArray;
 }
 
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+
+
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
